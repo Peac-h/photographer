119a38e69f4b152ac2d76685d346842f95db244d
_Peach <p6eac.h@gmail.com>
2024-06-21 17:25:28 +0400

    Add footer

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -1,1 +1,12 @@
-export const Footer = () => <footer></footer>;
+import { Link } from "react-router-dom";
+
+const currentYear = new Date().getFullYear();
+
+export const Footer = () => (
+  <footer className="flex justify-between border-t border-[#222] px-4 py-2 text-stone-700">
+    <Link to="/about" className="underline-offset-2 hover:underline">
+      About
+    </Link>
+    <span>© {currentYear}</span>
+  </footer>
+);
