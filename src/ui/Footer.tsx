6a3cb85f3c7efeb050c28c9fc3f2f0c08da31045
_Peach <p6eac.h@gmail.com>
2024-06-24 17:44:35 +0400

    Fix footer zIndex; Adjust text decoration color; Add public/_redirects

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const currentYear = new Date().getFullYear();
 
 export const Footer = () => (
-  <footer className="relative z-50 flex justify-between border-t border-stone-200 px-4 py-2 text-stone-400 dark:border-stone-800 dark:text-stone-700">
+  <footer className="relative z-30 flex justify-between border-t border-stone-200 px-4 py-2 text-stone-400 dark:border-stone-800 dark:text-stone-700">
     <Link
       to="/about"
       className="underline-offset-2 transition-all duration-200 ease-in-out hover:underline hover:decoration-stone-400 dark:hover:decoration-stone-700"
